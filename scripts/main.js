69685aad9eac8ed7010d511e5148156e1dfbbc8f
NelsonT4 <netory4@gmail.com>
2024-10-24 20:56:56 -0500

    main.js: Alerta por pinchasos

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,5 @@
 const miTitulo = document.querySelector("h1");
 miTitulo.textContent = "Hola Mundo";
+document.querySelector("html").onclick = function() {
+    alert("Ouch !Deja de Pincharme!");
+};
